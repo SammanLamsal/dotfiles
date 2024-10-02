533eb73bf27b427c606c00d3cc50bca497637ba1
Samman Lamsal <85963253+SammanLamsal@users.noreply.github.com>
2024-10-01 22:12:35 -0400

    Delete nvim/ftplugin directory

--- nvim/ftplugin/java.lua
+++
@@ -1,5 +0,0 @@
-local config = {
-    cmd = {'/home/linuxbrew/.linuxbrew/Cellar/jdtls/1.39.0/bin/jdtls'},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-}
-require('jdtls').start_or_attach(config)
