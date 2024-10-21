6cdbb427e9e92708600bde7c7df069bdb9ec8fa6
Samman Lamsal <85963253+SammanLamsal@users.noreply.github.com>
2024-10-20 23:26:46 -0400

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require("config.core")
-require("config.plugin")
-
---vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
---vim.cmd('highlight NonText guibg=NONE ctermbg=NONE')
---vim.cmd('highlight LineNr guibg=NONE ctermbg=NONE')
---vim.cmd('highlight EndOfBuffer guibg=NONE ctermbg=NONE')
