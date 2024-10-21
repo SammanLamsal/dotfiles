65cd087e937dbe49de75d424aedb5a2d70e4a6ab
Samman Lamsal <lamsal.samman@gmail.com>
2024-10-21 14:12:45 -0400

    Made neovim and nvim-tree follow terminal transparency

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 require("config.core")
 require("config.plugin")
 
---vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
---vim.cmd('highlight NonText guibg=NONE ctermbg=NONE')
---vim.cmd('highlight LineNr guibg=NONE ctermbg=NONE')
---vim.cmd('highlight EndOfBuffer guibg=NONE ctermbg=NONE')
+vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
+vim.cmd('highlight NonText guibg=NONE ctermbg=NONE')
+vim.cmd('highlight LineNr guibg=NONE ctermbg=NONE')
+vim.cmd('highlight EndOfBuffer guibg=NONE ctermbg=NONE')
--- .config/nvim/lua/config/plugins/nvim-tree.lua
+++ .config/nvim/lua/config/plugins/nvim-tree.lua
@@ -22,5 +22,9 @@ return {
         vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
         vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
         vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+
+        vim.cmd([[
+            :hi NvimTreeNormal guibg=NONE ctermbg=NONE
+        ]])
     end,
 }
