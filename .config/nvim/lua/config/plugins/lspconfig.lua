b7866289acbd5cced19169e124b201cb35574355
Samman Lamsal <lamsal.samman@gmail.com>
2024-10-26 11:31:58 -0400

    Added marksman lsp server

--- .config/nvim/lua/config/plugins/lspconfig.lua
+++ .config/nvim/lua/config/plugins/lspconfig.lua
@@ -1,11 +1,13 @@
 return {
     {
         "neovim/nvim-lspconfig",
+
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({capabilities = capabilities})
             lspconfig.pyright.setup({capabilities = capabilities})
+            lspconfig.marksman.setup({capabilities = capabilities})
         end,
     }
 }
