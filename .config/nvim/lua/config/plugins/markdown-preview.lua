fbaa638edda1e49bedfbc0554463e8870608249d
Samman Lamsal <lamsal.samman@gmail.com>
2024-10-26 18:24:44 -0400

    Added keybind for opening markdown preview

--- .config/nvim/lua/config/plugins/markdown-preview.lua
+++ .config/nvim/lua/config/plugins/markdown-preview.lua
@@ -5,6 +5,7 @@ return {
         build = "cd app && yarn install",
         init = function()
             vim.g.mkdp_filetypes = { "markdown" }
+            vim.keymap.set("n", "<leader>md", "<cmd>MarkdownPreview<CR>", { desc = "Open markdown preview" }) -- toggle file explorer
         end,
         ft = { "markdown" },
     },
