d7a2b7cd90029451ab71243b33a2c2f386f7c400
Samman Lamsal <lamsal.samman@gmail.com>
2024-10-26 12:42:21 -0400

    Ensure markdown parser installed

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -1,12 +1,12 @@
   return {
       "nvim-treesitter/nvim-treesitter",
       config = function()
-	      require("nvim-treesitter.configs").setup({
-		ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "java"},
-		auto_install = true,
-		highlight = {
-		  enable = true,
-		},
-	      })
+          require("nvim-treesitter.configs").setup({
+              ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "python", "java", "markdown", "markdown_inline"},
+              auto_install = true,
+              highlight = {
+                  enable = true,
+              },
+          })
       end,
   }
