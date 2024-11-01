1b17e87d4b1bac257ee962a83e76db820d848208
Samman Lamsal <lamsal.samman@gmail.com>
2024-11-01 19:55:25 -0400

    Made java.lua use environment variables

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,14 +1,16 @@
+local jdtls_config_path = os.getenv("JDTLS_CONFIG_PATH")
+
 local config = {
-    cmd = {'/home/linuxbrew/.linuxbrew/Cellar/jdtls/1.39.0/bin/jdtls'},
+    cmd = {jdtls_config_path},
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
-config.settings = {
-    java = {
-        project = {
-            referencedLibraries = {
-                '/mnt/c/Users/slok1/Desktop/Code/School/CS310/Project 2/p2-v2/yourCodeHere/opencsv-3.9.jar',
-            },
-        }
-    }
-}
+-- config.settings = {
+--     java = {
+--         project = {
+--             referencedLibraries = {
+--                 '/mnt/c/Users/slok1/Desktop/Code/School/CS310/Project 2/p2-v2/yourCodeHere/opencsv-3.9.jar',
+--             },
+--         }
+--     }
+-- }
 require('jdtls').start_or_attach(config)
