906d4b7b736efaadb10f533e5925823b8f236034
Samman Lamsal <lamsal.samman@gmail.com>
2024-11-01 20:03:50 -0400

    Fixed jdtls path

--- env_vars.sh
+++ env_vars.sh
@@ -3,11 +3,12 @@ case "$OSTYPE" in
         eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
         eval "$(dircolors -b)"
         
+        # on wsl, still need to put .wezterm.lua in WINDOWS home directory
         export WEZTERM_CONFIG_FILE="$HOME/wezterm_linux.lua"
-        export JDTLS_CONFIG_PATH="/home/linuxbrew/.linuxbrew/Cellar/jdtls/1.39.0/bin/jdtls"
+        export JDTLS_CONFIG_PATH="/home/linuxbrew/.linuxbrew/bin/jdtls"
     ;;
     darwin*)
         export WEZTERM_CONFIG_FILE="$HOME/wezterm_mac.lua"
-        export JDTLS_CONFIG_PATH="/home/linuxbrew/.linuxbrew/Cellar/jdtls/1.39.0/bin/jdtls"
+        export JDTLS_CONFIG_PATH="/opt/homebrew/bin/jdtls"
     ;;
 esac
