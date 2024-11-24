040d8ea6047f6d80a62b12adcf2a9d6c3aca3cd5
Samman Lamsal <lamsal.samman@gmail.com>
2024-11-23 22:43:06 -0500

    Make lsps load on BufReadPre and BufNewFile

--- .config/nvim/lua/config/plugins/lspconfig.lua
+++ .config/nvim/lua/config/plugins/lspconfig.lua
@@ -1,7 +1,7 @@
 return {
     {
         "neovim/nvim-lspconfig",
-
+        event = {"BufReadPre", "BufNewFile"},
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
