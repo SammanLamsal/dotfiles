e68706751ca9c979aee82506394e50bb77ed6d78
Samman Lamsal <lamsal.samman@gmail.com>
2025-09-07 23:54:22 -0400

    add new colorscheme to test out

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
     {
-        "rebelot/kanagawa.nvim",
-        -- config = function()
+        -- "rebelot/kanagawa.nvim",
+        -- -- config = function()
         -- vim.cmd.colorscheme("kanagawa-wave")
         -- end,
     },
     {
-        "catppuccin/nvim",
+        -- "catppuccin/nvim",
         -- name = "catppuccin",
         -- priority = 1000,
         -- config = function()
@@ -26,6 +26,15 @@ return {
             require("everforest").setup({})
             vim.cmd.colorscheme("everforest")
         end,
-    }
+    },
+    {
+        -- "vague2k/vague.nvim",
+        -- lazy = false,        -- make sure we load this during startup if it is your main colorscheme
+        -- priority = 1000,     -- make sure to load this before all the other plugins
+        -- config = function()
+        --     require("vague").setup({})
+        --     vim.cmd("colorscheme vague")
+        -- end
+    },
 
 }
