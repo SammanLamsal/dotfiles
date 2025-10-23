15690da0edc3b9a9237b71e2d53303d1cabba29d
Samman Lamsal <lamsal.samman@gmail.com>
2025-10-22 22:12:36 -0400

    add opts to keymaps

--- .config/nvim/lua/config/keys.lua
+++ .config/nvim/lua/config/keys.lua
@@ -1,1 +1,3 @@
-vim.keymap.set("n", "<leader>sd", vim.diagnostic.open_float)
+local opts = {noremap = true, silent = true}
+local map = vim.keymap.set
+map("n", "<leader>sd", vim.diagnostic.open_float, opts) -- show diagnostic
