4f8bc358cda3a3b4c9e453c26199244c2c7d9e73
Samman Lamsal <lamsal.samman@gmail.com>
2026-06-13 16:46:25 -0400

    treesitter fix x3

--- shared/.config/nvim/lua/plugins/treesitter.lua
+++ shared/.config/nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,11 @@
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
-    config = function()
-      local ts = require 'nvim-treesitter'
-      local parsers = {"c", "lua", "vim", "vimdoc", "query", "python", "java", "markdown", "markdown_inline"}
-      for _, parser in ipairs(parsers) do
-          ts.install(parser)
-      end
-    end
+    -- config = function()
+    --   local ts = require 'nvim-treesitter'
+    --   local parsers = {"c", "lua", "vim", "vimdoc", "query", "python", "java", "markdown", "markdown_inline"}
+    --   for _, parser in ipairs(parsers) do
+    --       ts.install(parser)
+    --   end
+    -- end
   }
